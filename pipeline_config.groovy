31ca3f47aacd27b77f3aa8fbf9f481e96050c341
samsonl <lyndon.samson@gmail.com>
2020-04-12 15:45:32 +1000

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -14,9 +14,5 @@ template_methods{
     unit_test
     static_code_analysis
     build
-    scan_container_image
-    penetration_test
-    accessibility_compliance_test
-    performance_test
-    functional_test
+    xxx
 }
